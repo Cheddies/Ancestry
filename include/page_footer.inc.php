1176a9e4a709c79f0818f608eefe52e2561bec85
Chris Gledhill <chris@corefulfilment.com>
2018-06-07 08:54:48 +0100

    GDPR Privacy & TCS
    Signed-off-by: Chris Gledhill <chris@corefulfilment.com>

--- include/page_footer.inc.php
+++ include/page_footer.inc.php
@@ -2,10 +2,12 @@
 	<ul id="footer_links" class="grid_7 alpha omega">
 		<li class="first"><a href="<?php echo WEBROOT;?>aboutus" title="About Us">About Us</a></li>
 		<li><a href="<?php echo WEBROOT;?>customer_service#contact" title="Contact Us">Contact Us</a></li>
-		<li><a href="http://www.ancestry.co.uk/home/partner/uk/uk_affiliate_home.aspx" title="Affiliate Programme">Affiliate Programme</a></li>
+		<li><a href="http://www.ancestry.co.uk/home/partner/uk/uk_affiliate_home.aspx" title="Affiliate Programme" target="_blank">Affiliate Programme</a></li>
 		<li><a href="http://www.ancestry.co.uk/advertising/uk/default.aspx" title="Advertising">Advertising</a></li>
-		<li><a href="<?php echo WEBROOT;?>privacy_policy" title="Privicy Statement">Privacy Statement</a></li>
-		<li class="last"><a href="<?php echo WEBROOT;?>tcs" title="Terms and Conditions">Terms and Conditions</a></li>
+		<!--<li><a href="<?php echo WEBROOT;?>privacy_policy" title="Privicy Statement">Privacy Statement</a></li>-->
+		<li><a href="https://www.ancestry.co.uk/cs/legal/privacystatement" title="Privicy Statement" target="_blank">Privacy Statement</a></li>
+		<!--<li class="last"><a href="<?php echo WEBROOT;?>tcs" title="Terms and Conditions">Terms and Conditions</a></li>-->
+		<li class="last"><a href="https://www.ancestry.co.uk/cs/legal/termsandconditions" title="Terms and Conditions" target="_blank">Terms and Conditions</a></li>
 	</ul>
 	<div id="other_sites" class="grid_3 alpha omega">
 		<p>Visit other Ancestry sites:</p>
