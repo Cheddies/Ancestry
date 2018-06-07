4bdbf7d37dc67bd299de8d1dd155bac56aab5fe1
Chris Gledhill <chris@corefulfilment.com>
2018-06-07 10:59:03 +0100

    GDPR Privacy & TSC

--- certificate/include/footer.php
+++ certificate/include/footer.php
@@ -18,8 +18,9 @@
 </div>
 <div id="shop_footer">
 <p><a href="/aboutus" onclick="s_objectID='corp_foot'">About Us</a>&nbsp;&nbsp;<a href="http://www.ancestryshop.co.uk/customer_service.php?#contact" onclick="s_objectID='contact_foot'" >Contact Us</a></p>
-			<p class="tnclinks">&copy;&nbsp;2017,&nbsp;Ancestry.com &nbsp;
-				<a id="ctl08_ctl00_m_privacy" onclick="s_objectID='privacy_foot'" href="http://www.ancestryshop.co.uk/privacy_policy.php">Privacy Policy</a> | <a id="ctl08_ctl00_m_terms" onclick="s_objectID='terms_foot'" href="http://www.ancestryshop.co.uk/tcs.php">Terms and Conditions</a>
+			<p class="tnclinks">&copy;&nbsp;2018,&nbsp;Ancestry.com &nbsp;
+				<!--<a id="ctl08_ctl00_m_privacy" onclick="s_objectID='privacy_foot'" href="http://www.ancestryshop.co.uk/privacy_policy.php">Privacy Policy</a> | <a id="ctl08_ctl00_m_terms" onclick="s_objectID='terms_foot'" href="http://www.ancestryshop.co.uk/tcs.php">Terms and Conditions</a>-->
+				<a id="ctl08_ctl00_m_privacy" onclick="s_objectID='privacy_foot'" href="https://www.ancestry.co.uk/cs/legal/privacystatement" title="Privicy Statement" target="_blank">Privacy Policy</a> | <a id="ctl08_ctl00_m_terms" onclick="s_objectID='terms_foot'" href="https://www.ancestry.co.uk/cs/legal/termsandconditions" title="Terms and Conditions" target="_blank">Terms and Conditions</a>
 			</p>
 			
 				<div class="genlinkdiv">
